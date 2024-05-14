c385d48bdfa006b6e7523792ad1dd7637b9887c2
ahuston-0 <aliceghuston@gmail.com>
2024-05-14 09:33:39 -0400

    add futures, remove logging for now
    Signed-off-by: ahuston-0 <aliceghuston@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,18 @@
 use log::{self, LevelFilter};
-use simple_logger::SimpleLogger;
+// use simple_logger::SimpleLogger; //
 use std::{
     fs::File,
     io::{self, prelude::*, BufReader},
     path::Path,
 };
 
-pub fn init_logs() {
-    SimpleLogger::new()
-        .with_level(LevelFilter::Info)
-        .env()
-        .init()
-        .unwrap();
-}
+// pub fn init_logs() {
+//     SimpleLogger::new()
+//         .with_level(LevelFilter::Info)
+//         .env()
+//         .init()
+//         .unwrap();
+// }
 
 pub mod prelude {
     // pub use itertools::Itertools;
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,21 @@
-use hydra_check_action_rs::{init_logs, prelude::*};
+// use hydra_check_action_rs::{init_logs, prelude::*};
 use std::env;
 use std::fs::write;
 use std::process::exit;
+use futures::executor::block_on;
+use reqwest::{Error,Response};
+
+async fn async_main(args: Vec<String>) -> String {                                               >{
+    let body = reqwest::get("https://www.rust-lang.org")
+        .await?
+        .text()
+        .await?;
+    return body;
+}
 
 fn main() {
-
-    let github_output_path = env::var("GITHUB_OUTPUT").unwrap();
+    let github_output_path =
+        env::var("GITHUB_OUTPUT").unwrap_or("/var/logs/hydra-check-action.log".to_string());
 
     let args: Vec<String> = env::args().collect();
     let error = &args[1];
@@ -15,4 +25,6 @@ fn main() {
         write(github_output_path, format!("error={error}")).unwrap();
         exit(1);
     }
+
+    write(github_output_path,format!(async_main(args).unwrap()));
 }
