649c03e5dafad32cd765112e3d4914fa99d999e2
ahuston-0 <aliceghuston@gmail.com>
2024-06-17 18:16:06 -0400

    add reqwest wrapper to hydra api
    Signed-off-by: ahuston-0 <aliceghuston@gmail.com>

--- src/hydra/hydra_api.rs
+++ src/hydra/hydra_api.rs
@@ -1,60 +1,66 @@
 use reqwest::{
-    blocking::{get, Response},
+    blocking::{Client,  Response},
+    header::ACCEPT,
     Result,
 };
 
 use super::hydra_builder::HydraInstance;
 
+
+fn get_wrapper(url: &str) -> Result<Response> {
+    Client::new().get(url).header(ACCEPT,"application/json").send()
+}
+
 pub fn get_projects(hydra_instance: HydraInstance) -> Result<Response> {
-    get(hydra_instance.hydra_url)
+    get_wrapper(hydra_instance.hydra_url.as_str())
 }
 
 pub fn get_jobsets(hydra_instance: HydraInstance) -> Result<Response> {
-    get(format!(
+    get_wrapper(format!(
         "{}/api/jobsets?project={}",
         hydra_instance.hydra_url, hydra_instance.project
-    ))
+    ).as_str())
 }
 
 pub fn get_project_by_name(hydra_instance: HydraInstance) -> Result<Response> {
-    get(format!(
+    get_wrapper(format!(
         "{}/projects/{}",
         hydra_instance.hydra_url, hydra_instance.project
-    ))
+    ).as_str())
 }
 
 pub fn get_jobset(hydra_instance: HydraInstance) -> Result<Response> {
-    get(format!(
+    get_wrapper(format!(
         "{}/jobset/{}/{}",
         hydra_instance.hydra_url,
         hydra_instance.project,
         hydra_instance.jobset.unwrap()
-    ))
+    ).as_str())
 }
 
 pub fn get_jobset_evals(hydra_instance: HydraInstance) -> Result<Response> {
-    get(format!(
+    get_wrapper(format!(
         "{}/jobset/{}/{}/evals",
         hydra_instance.hydra_url,
         hydra_instance.project,
         hydra_instance.jobset.unwrap()
-    ))
+    ).as_str())
 }
 
 pub fn get_jobset_evals_paginated(hydra_instance: HydraInstance, page: u64) -> Result<Response> {
-    get(format!(
+    get_wrapper(format!(
         "{}/jobset/{}/{}/evals?page={}",
         hydra_instance.hydra_url,
         hydra_instance.project,
         hydra_instance.jobset.unwrap(),
         page
-    ))
+    ).as_str())
 }
 
 pub fn get_build(hydra_instance: HydraInstance) -> Result<Response> {
-    get(format!(
+    get_wrapper(format!(
         "{}/build/{}",
         hydra_instance.hydra_url,
         hydra_instance.build.unwrap()
-    ))
+    ).as_str())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use hydra_check_action_rs::hydra::hydra_builder::HydraInstanceBuilder;
+use hydra_check_action_rs::hydra::{hydra_builder::HydraInstanceBuilder,
+                                   hydra_api};
 use std::env;
 use std::fs::write;
 use std::process::exit;
@@ -8,22 +9,26 @@ fn main() {
         env::var("GITHUB_OUTPUT").unwrap_or("/var/log/hydra-check-action.log".to_string());
 
     let args: Vec<String> = env::args().collect();
-    let error = &args[1];
+    if args.len() >= 2 {
+        let error = &args[1];
 
-    if !error.is_empty() {
-        eprintln!("Error: {error}");
-        write(github_output_path, format!("error={error}")).unwrap();
-        exit(1);
+        if !error.is_empty() {
+            eprintln!("Error: {error}");
+            write(github_output_path, format!("error={error}")).unwrap();
+            exit(1);
+        }
     }
 
-    let _hydra_instance = HydraInstanceBuilder::new()
+    let hydra_instance = HydraInstanceBuilder::new()
         .hydra_url(String::from("https://hydra.alicehuston.xyz"))
         .project(String::from("nix-dotfiles-build"))
         .jobset(String::from("branch-main"))
         .build();
-    // let hydra_instance = "https://hydra.alicehuston.xyz";
-    // let body = get_projects(hydra_instance).unwrap();
 
+    let body = hydra_api::get_projects(hydra_instance).unwrap().text().unwrap();
+
+
+    println!("{}",body);
     // error!("{:#?}",body);
     // let _ = write(github_output_path, format!("hi"));
 }
